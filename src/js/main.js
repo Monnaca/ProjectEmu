ef2b337912dd3e936700a5b4643e774c142f4dd3
root <root@DESKTOP-93NTFJF>
2023-09-12 14:56:05 +0700

    God help me

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
+const winWitdh = window.innerWidth
+const winHeight = window.innerHeight
+
 document.addEventListener('DOMContentLoaded', function() {
-    var animatedDiv = document.getElementById('animatedDiv');
+    const animatedDiv = document.getElementById('animatedDiv')
     animatedDiv.addEventListener('animationend', function() {
         console.log('Animation ended');
-    });
-});
+    })
+})
