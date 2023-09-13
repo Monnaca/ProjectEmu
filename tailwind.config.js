cd6014b0efb718ffd7e3dc7a298cfc5dc27efbd0
root <root@DESKTOP-93NTFJF>
2023-09-13 14:19:39 +0700

    All hope is lost

---
+++ tailwind.config.js
@@ -0,0 +1,73 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/js/*.js"
+    ],
+    theme: {
+        extend: {
+            colors : {
+                "dbg" : "#212121",
+                "lbg" : "#E0E0E0",
+            },
+            space: {
+                '5px': '5px'
+            },
+            animation: {
+                'blink': 'blink .6s linear infinite alternate',
+                'cday' : 'cday 2000ms linear',
+                'cnight' : 'cnight 2s linear'
+            },
+            keyframes: {
+                blink: {
+                    '0%' : {'opacity': '1'},
+                    '40%' : {'opacity': '1'},
+                    '60%' : {'opacity': '0'},
+                    '100%' : {'opacity': '0'},
+                },
+                cday: {
+                    '0%' : {'background-color': '#2b2b2b', 'opacity' : '1'},
+                    '50%' : {'background-color': '#2b2b2b', 'opacity' : '1'},
+                    '75%' : {'background-color' : '#ffffff', 'opacity' : '1'},
+                    '100%' : {'background-color' : '#ffffff', 'opacity' : '1'},
+                },
+                cnight : {
+                    '0%' : {'background-color': '#ffffff'},
+                    '50%' : {'background-color' :'#ffffff'},
+                    '75%' : {'background-color' : '#2b2b2b'},
+                    '100%' : {'background-color' : '#2b2b2b'},
+                },
+                rotateIn : {
+                    '0%' : {
+                        "-webkit-transform" : "rotate(-45deg)",
+                        "transform" : "rotate(-45deg)",
+                        "-webkit-transform-origin" : "100% 0",
+                        "transform-origin" : "100% 0",
+                        "opacity" : "0"
+                    },
+                    '100%' : {
+                        "-webkit-transform" : "rotate(0)",
+                        "transform" : "rotate(0)",
+                        "-webkit-transform-origin" : "0 0",
+                        "transform-origin" : "0 0",
+                        "opacity" : "1"
+                    }
+                },
+                slideInBottom: {
+                    '0%' : {
+                        "transform" : "translateY(1000px)",
+                        "-webkit-transform" : "translateY(1000px)",
+                        "opacity" : "0",
+                    },
+                    '100%' : {
+
+                    }
+                }
+            }
+        },
+        fontFamily: {
+            nunito: ['"Nunito Sans"', 'sans-serif']
+        }
+    },
+    plugins: []
+}
